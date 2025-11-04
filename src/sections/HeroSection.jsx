b04d93b96fa32b70affe9433df1b0aa53e8fd3fc
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-11-04 22:26:24 +0100

    feat: added animation to herosection

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,4 +1,25 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 function HeroSection() {
+	useGSAP(() => {
+		const heroTl = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".hero-container",
+				start: "top top",
+				end: "bottom top",
+				scrub: true,
+				// markers: true,
+			},
+		});
+		heroTl.to(".hero-container", {
+			rotate: 7,
+			scale: 0.85,
+			y: 300,
+			ease: "power1.inOut",
+		});
+	});
+
 	return (
 		<section className="bg-main-bg">
 			<div className="hero-container">
