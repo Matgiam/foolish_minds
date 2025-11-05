b68570a3a03a25e76d9515afb37c910369412b9e
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-11-05 19:09:38 +0100

    add: added text-animations in the HeroSection

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,8 +1,23 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import Navbar from "../components/Navbar";
 
 function HeroSection() {
 	useGSAP(() => {
+		const titleSplit = SplitText.create(".general-title", { type: "chars" });
+		gsap.from(titleSplit.chars, {
+			yPercent: 50,
+			opacity: 0,
+			ease: "power1.inOut",
+			stagger: 0.02,
+		});
+		gsap.from("p", {
+			yPercent: 30,
+			opacity: 0,
+			delay: 0.3,
+			ease: "power1.inOut",
+		});
 		const heroTl = gsap.timeline({
 			scrollTrigger: {
 				trigger: ".hero-container",
@@ -21,7 +36,7 @@ function HeroSection() {
 	});
 
 	return (
-		<section className="bg-main-bg">
+		<section className="bg-main-bg" id="hero">
 			<div className="hero-container">
 				<img src="/images/homescreen.png" alt="" className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-100" />
 				<div className="hero-content text-white">
