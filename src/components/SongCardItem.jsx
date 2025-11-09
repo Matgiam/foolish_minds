fdbc1a682e12e5f01ff39ed681ddaf983e78a4d9
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-11-09 21:51:18 +0100

    feat: added the pause-icon

--- src/components/SongCardItem.jsx
+++ src/components/SongCardItem.jsx
@@ -1,7 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function SongCardItem(props) {
 	const audioRef = useRef(null);
+	const [isPlaying, setIsPlaying] = useState(false);
 
 	const handlePlay = () => {
 		const allAudios = document.querySelectorAll("audio");
@@ -11,12 +12,16 @@ function SongCardItem(props) {
 				audio.currentTime = 0;
 			}
 		});
-
 		if (audioRef.current.paused) {
 			audioRef.current.play();
+			setIsPlaying(true);
 		} else {
 			audioRef.current.pause();
+			setIsPlaying(false);
 		}
+	};
+	const handleEnded = () => {
+		setIsPlaying(false);
 	};
 
 	return (
@@ -31,10 +36,15 @@ function SongCardItem(props) {
 						<h2 className="second-title">{props.title}</h2>
 						<p>{props.artist}</p>
 					</div>
-					<img src="/images/play-icon.png" className="md:w-14 h-16 pt-3 cursor-pointer" alt="Play" onClick={handlePlay} />
+					<img
+						src={isPlaying ? "/images/pause-icon.png" : "/images/play-icon.png"}
+						className="md:w-14 h-16 pt-3 cursor-pointer"
+						onClick={handlePlay}
+					/>
 				</div>
 			</div>
-			<audio ref={audioRef} src={props.audioSrc} />
+
+			<audio ref={audioRef} src={props.audioSrc} onEnded={handleEnded} />
 		</div>
 	);
 }
